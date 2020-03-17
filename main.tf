6a6517bedffcc29d2a418ed4b9e183f3d9d5e66d
Marco Bergantin <marco@bergant.in>
2020-03-17 10:43:25 +0100

    add block public access policy

--- main.tf
+++ main.tf
@@ -25,3 +25,12 @@ resource "aws_s3_bucket" "logs" {
 
   tags = var.tags
 }
+
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
